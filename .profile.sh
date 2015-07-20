b68f0a17e68eb48c2aac7b0877a3c21e56029051
George Jor <georgejor@maaii.com>
2015-07-20 15:27:35 +0800

    add git prompt and alias

---
+++ .profile.sh
@@ -0,0 +1,8 @@
+# Add this to ~/.bash_profile for mounting
+#  . "$(pwd)/automator/.profile.sh"
+
+git config --global core.excludesfile ./bash/.gitignore
+
+. ~/automator/bash/.colors.sh
+. ~/automator/bash/.prompt.sh
+. ~/automator/bash/.alias.sh
--- bash/.alias.sh
+++ bash/.alias.sh
@@ -1,9 +1,23 @@
-alias ls='ls -a'
+alias ls='ls -al'
 alias home='cd ~'
-alias ..='cd ..'
 alias ~='cd ~'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
 
 # Git related
-alias gs='git status -sb'
-alias gf='git fetch'
-alias gb='git branch'
+alias log='git log'
+alias status='git status -sb'
+alias fetch='git fetch'
+alias branch='git branch'
+alias push='git push origin master'
+alias pull='git pull origin master'
+
+# Maaii special
+alias mpull='git pull origin bolt'
+alias mpush='git push origin @:refs/for/bolt'
+alias wlp='cd ~/Maaii/m800-white-label-portal'
+
+# Project related
+alias auto='cd ~/automator'
