602848d2e005e0008aeb46caf7b8bced816ea6af
George Jor <yhjor@GeorgedeMacBook-Pro.local>
2015-09-01 21:26:02 +0800

    update profile.sh

--- .profile.sh
+++ .profile.sh
@@ -1,9 +1,16 @@
-# Add this to ~/.bash_profile for mounting
-#  . "$(pwd)/automator/.profile.sh"
+# 1. Add this to ~/.bashrc for mounting in case of your automator folder is under a folder named projects
+#  . ~/projects/automator/.profile.sh
+
+# 2. Add
+# if [ -f ~/.bashrc ]; then
+#    source ~/.bashrc
+# fi
+# to ~/.bash_profile
+
 
 git config --global core.excludesfile ./bash/.gitignore
 
-. ~/automator/bash/.colors.sh
-. ~/automator/bash/.prompt.sh
-. ~/automator/bash/.alias.sh
-. ~/automator/bash/.projects.sh
+. ~/projects/automator/bash/.colors.sh
+. ~/projects/automator/bash/.prompt.sh
+. ~/projects/automator/bash/.alias.sh
+. ~/projects/automator/bash/.projects.sh
