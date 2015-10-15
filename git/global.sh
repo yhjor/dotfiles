4bd7839d0a9fb015dcae398c4c355481a6ae5cf4
George Jor <georgejor@maaii.com>
2015-10-15 10:58:54 +0800

    Move all git shell alias to git alias

--- git/global.sh
+++ git/global.sh
@@ -4,8 +4,12 @@ git config --global core.ignorecase false
 
 # alias related
 git config --global alias.co checkout
-git config --global alias.logs 'log --oneline'
+git config --global alias.logs 'log --oneline --decorate'
 git config --global alias.rb 'rebase -i HEAD~'
+git config --global alias.rs 'reset --soft HEAD^'
+
+git config --global alias.s stash
+git config --global alias.sp 'stash pop'
 
 git config --global alias.mt mergetool
 git config --global alias.abort 'rebase --abort'
@@ -14,6 +18,9 @@ git config --global alias.continue 'rebase --continue'
 git config --global alias.cb 'checkout -b'
 git config --global alias.nb 'branch -m'
 git config --global alias.db 'branch -D'
+git config --global alias.origin 'branch -vv'
+
+git config --global alias.load '!sh -c "git checkout $1 && git fetch && git rebase -p origin/$1"'
 
 # colors related
 git config --global color.ui always
