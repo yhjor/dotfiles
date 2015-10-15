9053aaa0c06bf8f0ba15bc4f7369b47377e8122d
George Jor <georgejor@maaii.com>
2015-10-15 11:00:09 +0800

    Add trash alias commas and remove rm alias

--- alias/general.sh
+++ alias/general.sh
@@ -1,11 +1,3 @@
-rm_rf() {
-  if [ -z $1 ]; then
-    return 'Please enter files to be deleted'
-  else
-    rm -rf $1
-  fi
-}
-
 alias ls='ls -a'
 alias home='cd ~'
 alias ~='cd ~'
@@ -15,4 +7,4 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias reload='chmod +x ~/.bashrc && . ~/.bashrc'
 alias desktop='cd ~/Desktop && open .'
-alias rm=rm_rf
+alias t=trash
