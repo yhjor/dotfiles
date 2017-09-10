f869bcd29a7fe6a430c5c5fd6f8c4cb2363b9596
George Jor <yhjor@me.com>
2017-09-10 11:09:43 +0800

    Update setup flow

--- setup.sh
+++ setup.sh
@@ -3,19 +3,27 @@ ln -sf $(pwd)/dots/.alias ~
 ln -sf $(pwd)/dots/.gitconfig ~
 ln -sf $(pwd)/dots/.vimrc ~
 
-# Ensure admin account is accessible to brew pacakges
-sudo -v
-
-# Check if Homebrew is installed
-which -s brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Optimize Mac's default settings
+source setup/osx.sh
 
 # Setup the terminal for new Mac environment only
 if [ -n "$BASH_VERSION" ]; then
+  # Ensure admin account is accessible to brew pacakges
+  sudo -v
+  
+  # Install homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  # Update OS
   softwareupdate -iva
   xcode-select --install
+  
+  # Update existing git and java version
   brew install git
   brew cask install java
-	source setup/terminal.sh
+  
+  # Setting up ZSH
+  source setup/terminal.sh
 fi
 
 # Brew and Node global packages
@@ -24,6 +32,3 @@ source setup/tools.sh
 # Web Development
 which -s mongod || brew install mongodb && brew service start mongodb
 which -s redis-cli || brew install redis && brew services start redis
-
-# Optimize Mac's default settings
-source setup/osx.sh
